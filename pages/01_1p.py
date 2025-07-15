e080cbab560938c81d9cc675bf27e672c82faee9
akmgun2008 <hongu3416@gmail.com>
2025-07-15 11:11:47 +0900

    Update 01_1p.py

--- pages/01_1p.py
+++ pages/01_1p.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="EV Opinion", layout="centered")
 
+# 제목 - 글씨 3배, 단일 폰트, 굵기 없음
+st.markdown(
+    "<h1 style='font-size: 3em; font-weight: normal;'>내가 생각하는 전기차 전망</h1>",
+    unsafe_allow_html=True
+)
+
+# 본문 - 사용자 의견 그대로
 st.write("""
 전기차에 대한 전망은 앞으로도 우상향을 그릴것을 예상한다. 왜냐하면 전기차에대한 고질적인 문제들이 점점 해결되고 있기때문이다. 너무 조용하다던가 연료효율이 안 좋다던가 혹은 주유소가 적다던가 이러한 문제들이 기술발전과 함께 개선 되었고 정부도 전기차에대한 보조금도 주고 있어 기업들도 투자를 할 경향이 높다
 """)
